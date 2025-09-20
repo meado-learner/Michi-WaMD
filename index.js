4ff5d59974188311086a21a9b205c0adde4879dd
meado-learner <meadonix@gmail.com>
2025-09-20 12:36:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './settings.js'
-import './plugins/_allfake.js'
+import './plugins/_fakes.js'
 import cfonts from 'cfonts'
 import { createRequire } from 'module'
 import { fileURLToPath, pathToFileURL } from 'url'
