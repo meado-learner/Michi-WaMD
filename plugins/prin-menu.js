506941738557f238b244ae486d47d8db38e23327
meado-learner <meadonix@gmail.com>
2025-09-19 19:55:33 -0600

    Update prin-menu.js

--- plugins/prin-menu.js
+++ plugins/prin-menu.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     txt += `✦ Bot » ${(conn.user.jid == global.conn.user.jid ? 'Principal 🅥' : 'Sub Bot 🅑')}\n\n`
 
     for (let tag in menu) {
-      txt += `*${tag.toUpperCase()}*\n`
+      txt += `➭ *${tag.toUpperCase()}*\n`
       for (let plugin of menu[tag]) {
         
         for (let cmd of plugin.help) {
