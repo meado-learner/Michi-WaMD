e2844a10741e68ea9f504274bd7b4cb4ebbdad1c
meado-learner <meadonix@gmail.com>
2025-09-20 10:15:19 -0600

    MiniHub: Subida de plugins/_fakeReply.js

---
+++ plugins/_fakeReply.js
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch'
+
+export async function before(m, { conn }) {
+
+global.rcanal = {
+contextInfo: {
+isForwarded: true,
+forwardedNewsletterMessageInfo: {
+newsletterJid: id_canal,
+serverMessageId: 100,
+newsletterName: name_canal,
+}}}
+  
+}
