1e7507ed35732f55dc18f139f5cc437a0d3dcafc
meado-learner <meadonix@gmail.com>
2025-09-20 21:13:19 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,7 +54,7 @@ vreden: { url: "https://api.vreden.web.id", key: null },
 delirius: { url: "https://api.delirius.store", key: null },
 zenzxz: { url: "https://api.zenzxz.my.id", key: null },
 siputzx: { url: "https://api.siputzx.my.id", key: null },
-adonix: { url: "https://myapiadonix.casacam.net", key: null }
+adonix: { url: "https://api-adonix.ultraplus.click", key: null }
 }
 
 
