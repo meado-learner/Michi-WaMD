f13ae01d0241d6b5e0c2a0bcd23c6f0d90172233
meado-learner <meadonix@gmail.com>
2025-09-21 11:37:40 -0600

    Create stickers-s.js

---
+++ plugins/stickers-s.js
@@ -0,0 +1,59 @@
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
+
+let handler = async (m, { conn, args }) => {
+    let stiker = false
+    let userId = m.sender
+    let packstickers = global.db.data.users[userId] || {}
+    let texto2 = packstickers.text2 || global.packsticker2
+    let texto1 = ''
+
+    try {
+        let q = m.quoted ? m.quoted : m
+        let mime = (q.msg || q).mimetype || q.mediaType || ''
+        let txt = args.join(' ')
+
+        if (m.text && m.text.startsWith('#s ')) {
+            texto1 = m.text.replace('#s ', '').trim() || global.packsticker
+        } else if (m.text && m.text === '#s') {
+            texto1 = global.packsticker
+        } else {
+            texto1 = packstickers.text1 || global.packsticker
+        }
+
+        if (/webp|image|video/g.test(mime) && q.download) {
+            if (/video/.test(mime) && (q.msg || q).seconds > 16)
+                return conn.reply(m.chat, '✧ El video no puede durar más de *15 segundos*', m)
+
+            let buffer = await q.download()
+            await m.react('🕓')
+            let marca = [texto1, texto2]
+            stiker = await sticker(buffer, false, marca[0], marca[1])
+        } else if (args[0] && isUrl(args[0])) {
+            let buffer = await sticker(false, args[0], texto1, texto2)
+            stiker = buffer
+        } else {
+            return conn.reply(m.chat, '《✧》Por favor, envia una imagen o video para hacer un sticker.', m)
+        }
+    } catch (e) {
+        await conn.reply(m.chat, '⚠︎ Ocurrió un Error: ' + e.message, m)
+        await m.react('✖️')
+    } finally {
+        if (stiker) {
+            conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+            await m.react('✅')
+        }
+    }
+}
+
+handler.help = ['sticker']
+handler.tags = ['sticker']
+handler.command = ['s', 'sticker']
+
+export default handler
+
+const isUrl = (text) => {
+    return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)(jpe?g|gif|png)/, 'gi'))
+          }
