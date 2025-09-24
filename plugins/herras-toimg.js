2f46f18e86b2becd596a2a229a739dd3057dac4a
meado-learner <meadonix@gmail.com>
2025-09-24 06:24:03 -0600

    Create herras-toimg.js

---
+++ plugins/herras-toimg.js
@@ -0,0 +1,20 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+if (!m.quoted) {
+return conn.reply(m.chat, `ꕤ Debes citar un sticker para convertir a imagen.`, m, rcanal)
+}
+await m.react('🕒')
+let xx = m.quoted
+let imgBuffer = await xx.download()   
+if (!imgBuffer) {
+await m.react('✖️')
+return conn.reply(m.chat, `✩ No se pudo procesar el sticker.`, m)
+}
+await conn.sendMessage(m.chat, { image: imgBuffer, caption: '✿ *Aquí tienes :D*' }, { quoted: m })
+await m.react('✔️')
+}
+
+handler.help = ['toimg']
+handler.tags = ['herramientas']
+handler.command = ['toimg', 'jpg', 'img'] 
+
+export default handler
